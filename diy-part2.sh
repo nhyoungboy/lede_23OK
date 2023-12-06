95cec4ffa2c1e9a660d6630fd4b2e35a02978042
forseticn <63159639+forseticn@users.noreply.github.com>
2023-12-07 02:51:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,9 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# 设置默认IP地址
+sed -i 's/192.168.1.1/10.0.0.3/g' package/base-files/files/bin/config_generate
+
+# 清除登陆密码
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
