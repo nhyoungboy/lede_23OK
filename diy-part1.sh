b531e5cfe0bc1654135702947ef286557e6ec87f
forseticn <63159639+forseticn@users.noreply.github.com>
2024-03-06 12:39:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,8 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
