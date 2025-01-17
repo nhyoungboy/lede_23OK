a7a0188d34dafcfe161d36832c9b781abf01d118
nhyoungboy <118877332+nhyoungboy@users.noreply.github.com>
2025-01-17 19:03:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,20 +15,29 @@ rm -rf feeds/packages/net/mosdns
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-mosdns
 
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+  
+
 # 修复golang版本过低
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 
 # 科学上网插件
-echo 'src-git vernesong https://github.com/vernesong/OpenClash' >>feeds.conf.default
+git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 
 # MosDNS
-git clone --depth=1 https://github.com/sbwml/luci-app-mosdns  package/lean/luci-app-mosdns
+git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
 
 # 添加 onliner 插件
-git clone https://github.com/nhhqgirl/luci-app-onliner.git package/lean/luci-app-onliner
+git clone https://github.com/nhhqgirl/luci-app-onliner.git package/luci-app-onliner
 
 # Themes
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git_sparse_clone main https://github.com/haiibo/packages luci-theme-opentomcat
 
-# 添加 opentomcat 主题
-git clone --depth=1 https://github.com/nhyoungboy/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
