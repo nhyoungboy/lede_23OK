1a5914c5ac10a2dc5e25f9ecaec1b2ad09281e4c
nhyoungboy <118877332+nhyoungboy@users.noreply.github.com>
2025-01-17 15:45:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,6 +9,8 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
+echo 'src-git sbwml https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
+echo 'src-git vernesong https://github.com/vernesong/OpenClash' >>feeds.conf.default
 
 # 添加 onliner 插件
 git clone https://github.com/nhhqgirl/luci-app-onliner.git package/lean/luci-app-onliner
