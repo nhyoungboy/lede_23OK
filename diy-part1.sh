51de629d492726dd17220a567da9048e60218239
nhyoungboy <118877332+nhyoungboy@users.noreply.github.com>
2025-01-17 15:42:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,8 +13,4 @@
 # 添加 onliner 插件
 git clone https://github.com/nhhqgirl/luci-app-onliner.git package/lean/luci-app-onliner
 
-# 添加 PowerOff 插件
-git clone https://github.com/nhhqgirl/luci-app-poweroff.git package/lean/luci-app-poweroff
 
-# 添加 opentomcat 主题
-# git clone --depth=1 https://github.com/nhyoungboy/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
