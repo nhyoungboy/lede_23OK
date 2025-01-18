e4cf95b5ff738e415331f250426409735112d529
nhyoungboy <118877332+nhyoungboy@users.noreply.github.com>
2025-01-18 21:50:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,11 +10,12 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
+
 # 添加 onliner 插件
 git clone https://github.com/nhhqgirl/luci-app-onliner.git package/lean/luci-app-onliner
 
 # 添加 PowerOff 插件
-# git clone https://github.com/nhhqgirl/luci-app-poweroff.git package/lean/luci-app-poweroff
+git clone https://github.com/nhhqgirl/luci-app-poweroff.git package/lean/luci-app-poweroff
 
-# 添加 argon 主题
-git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+# 添加 opentomcat 主题
+git clone --depth=1 https://github.com/nhyoungboy/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
