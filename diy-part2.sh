9117faa589770eabd1326d69b0f5fa611fd99a11
nhyoungboy <118877332+nhyoungboy@users.noreply.github.com>
2025-01-18 11:10:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,4 +1,4 @@
-
+rm -rf feeds/luci/applications/luci-app-openclash
 # 修改网络设置
 sed -i 's/192.168.1.1/192.168.81.1/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.81.1/g' package/base-files/luci2/bin/config_generate
